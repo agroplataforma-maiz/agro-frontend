14b0237d17b95e0feea99deaec86bab0847bac0d
Alfredo Barron <alfreedobarron@gmail.com>
2026-03-26 00:54:58 -0600

    slash

--- js/catalogos.js
+++ js/catalogos.js
@@ -310,15 +310,15 @@ async function apiFetch(path, opciones = {}) {
 
 
 const getPrefix = (cat) => "catalogo";
-const listar = (cat) => apiFetch(`/${getPrefix(cat)}/${cat}/`);
-const obtener = (cat, id) => apiFetch(`/${getPrefix(cat)}/${cat}/${id}/`);
+const listar = (cat) => apiFetch(`/${getPrefix(cat)}/${cat}`);
+const obtener = (cat, id) => apiFetch(`/${getPrefix(cat)}/${cat}/${id}`);
 const crear = (cat, d) =>
-  apiFetch(`/${getPrefix(cat)}/${cat}/`, {
+  apiFetch(`/${getPrefix(cat)}/${cat}`, {
     method: "POST",
     body: JSON.stringify(d),
   });
 const actualizar = (cat, id, d) =>
-  apiFetch(`/${getPrefix(cat)}/${cat}/${id}/`, {
+  apiFetch(`/${getPrefix(cat)}/${cat}/${id}`, {
     method: "PUT",
     body: JSON.stringify(d),
   });
